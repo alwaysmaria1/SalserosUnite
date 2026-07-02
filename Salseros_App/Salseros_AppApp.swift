f35dbbcd4cdb585680e0b972279f2a4ccfbaf740
Noelia <alwaysmaria1@gmail.com>
2026-07-01 23:07:06 -0500

    Initial Commit

---
+++ Salseros_App/Salseros_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Salseros_AppApp.swift
+//  Salseros_App
+//
+//  Created by Noelia Herne on 7/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Salseros_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
